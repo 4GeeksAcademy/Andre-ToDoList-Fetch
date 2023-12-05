fc708881d92cc4597e483e92efa79ac368105c47
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2023-12-05 00:04:07 +0000

    funcionando ok!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,83 +1,123 @@
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  const [entradaTarea, setEntradaTarea] = useState("");
+  const [entradaTarea, setEntradaTarea] = useState('');
   const [nuevasTareas, setNuevasTareas] = useState([]);
   const [indiceHover, setIndiceHover] = useState(null);
 
   useEffect(() => {
-    getTodoList();
+    obtenerTareas();
   }, []);
 
-  async function manejarTeclaEnter(e) {
-    if (e.key === "Enter") {
-      try {
-        const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/andre.abreo");
+  useEffect(() => {
+    crearUsuario();
+  }, []);
+
+  const obtenerTareas = async () => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/AndreAbreo");
+
+      if (response.ok) {
         const data = await response.json();
-
-        if (data && data.data && Array.isArray(data.data) && data.data.length > 0) {
-          console.log("El usuario ya existe");
-        } else {
-          const nuevaTarea = { done: false, id: Date.now().toString(), label: entradaTarea };
-
-          const postResponse = await fetch("https://playground.4geeks.com/apis/fake/todos/user/andre.abreo", {
-            method: "POST",
-            body: JSON.stringify([nuevaTarea]), // Enviar como array con la nueva tarea
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-
-          const postData = await postResponse.json();
-
-          if (postData && postData.data && Array.isArray(postData.data)) {
-            setNuevasTareas(postData.data);
-          } else {
-            console.error("La respuesta del servidor no tiene el formato esperado:", postData);
-          }
-
-          setEntradaTarea("");
-        }
-      } catch (error) {
-        console.error("Error al procesar la solicitud:", error);
+        setNuevasTareas(data);
+      } else {
+        console.error("Error al obtener las tareas:", response.statusText);
       }
+    } catch (error) {
+      console.error("Error al procesar la solicitud de tareas:", error);
     }
-  }
-
-  function getTodoList() {
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/andre.abreo")
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log("Respuesta del servidor:", data);
-        if (data && data.data && Array.isArray(data.data)) {
-          setNuevasTareas(data.data);
-        } else {
-          console.error("La respuesta del servidor no tiene el formato esperado:", data);
-        }
-      })
-      .catch((error) => console.log(error));
-  }
-
-  const eliminarTarea = (id) => {
-    const eliminadas = nuevasTareas.filter((tarea) => tarea.id !== id);
-
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/andre.abreo", {
-      method: "PUT",
-      body: JSON.stringify(eliminadas),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data && data.data && Array.isArray(data.data)) {
-          setNuevasTareas(data.data);
-        } else {
-          console.error("La respuesta del servidor no tiene el formato esperado:", data);
-        }
-      })
-      .catch((error) => console.log(error));
   };
+
+  const crearUsuario = async () => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/AndreAbreo");
+  
+      if (response.status === 404) {
+        await fetch("https://playground.4geeks.com/apis/fake/todos/user/AndreAbreo", {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify([])
+        });
+        console.log("Usuario creado exitosamente.");
+      } else if (!response.ok) {
+        console.error("Error al crear o verificar el usuario:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error al procesar la solicitud de creación o verificación de usuario:", error);
+    }
+  };
+
+  const manejarEntradaTarea = (e) => {
+    setEntradaTarea(e.target.value);
+  };
+
+  const manejarTeclaEnter = async (e) => {
+    if (e.key === "Enter" && entradaTarea.trim() !== "") {
+      const nuevaTarea = { done: false, id: Date.now().toString(), label: entradaTarea };
+      setNuevasTareas([...nuevasTareas, nuevaTarea]);
+      await actualizarTareas([...nuevasTareas, nuevaTarea]);
+
+      setEntradaTarea("");
+    }
+  };
+
+  const actualizarTareas = async (tareas) => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/AndreAbreo", {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(tareas.map(({ id, label, done }) => ({ id, label, done }))),
+      });
+  
+      if (!response.ok) {
+        console.error("Error al actualizar las tareas:", response.statusText);
+      } else {
+        console.log("Tareas actualizadas exitosamente.");
+      }
+    } catch (error) {
+      console.error("Error al procesar la solicitud de actualización de tareas:", error);
+    }
+  };
+
+  const eliminarTarea = async (id) => {
+    let eliminadas = nuevasTareas.filter((tarea, index) => {
+      return index !== id;
+    });
+
+    setNuevasTareas(eliminadas);
+    await actualizarTareas(eliminadas);
+  };
+
+  const eliminarTodasLasTareas = async () => {
+    try {
+      if (nuevasTareas.length === 0) {
+        console.log("No hay tareas para eliminar.");
+        return;
+      }
+  
+      const response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/AndreAbreo",
+        {
+          method: "DELETE",
+        }
+      );
+  
+      if (response.ok) {
+        console.log("Todas las tareas eliminadas exitosamente.");
+        setNuevasTareas([]);
+        await crearUsuario(); // Vuelve a crear el usuario después de eliminar todas las tareas
+      } else {
+        console.error("Error al eliminar todas las tareas:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error al procesar la solicitud de eliminación de tareas:", error);
+    }
+  };
+  
 
   return (
     <div className="container containerEstilo">
@@ -85,22 +125,22 @@ const Home = () => {
       <input
         type="text"
         placeholder="¿Qué se debe hacer?"
-        onChange={(e) => setEntradaTarea(e.target.value)}
+        onChange={manejarEntradaTarea}
         value={entradaTarea}
         onKeyDown={manejarTeclaEnter}
         className="form-control entradaFormulario"
       />
       <ul className="todo-list listaDeTareas">
-        {nuevasTareas.map((tarea) => (
+        {nuevasTareas.map((tarea, i) => (
           <li
-            key={tarea.id}
-            onMouseEnter={() => setIndiceHover(tarea.id)}
+            key={i}
+            onMouseEnter={() => setIndiceHover(i)}
             onMouseLeave={() => setIndiceHover(null)}
             className="todo-item elementoDeTarea"
           >
             <span className="textoTarea">{tarea.label}</span>
-            {indiceHover === tarea.id && (
-              <button onClick={() => eliminarTarea(tarea.id)} className="btnEliminar">
+            {indiceHover === i && (
+              <button onClick={() => eliminarTarea(i)} className="btnEliminar">
                 Eliminar
               </button>
             )}
@@ -108,8 +148,14 @@ const Home = () => {
         ))}
       </ul>
       <div>{nuevasTareas.length} Items Pendientes</div>
+      <button onClick={eliminarTodasLasTareas} className="btnEliminar">
+        Eliminar Todas las Tareas
+      </button>
     </div>
   );
 };
 
 export default Home;
+
+
+
